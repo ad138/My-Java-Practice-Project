7c3011d696c3a4583f7ca2400a31cd558cb82ef4
Aparna Das <adas138@gmail.com>
2016-05-23 19:29:19 +0530

    New arraylist with student type argument

--- com.myjavapractice.19.05.16/src/collectionFrameWork/ArrayListClass.java
+++ com.myjavapractice.19.05.16/src/collectionFrameWork/ArrayListClass.java
@@ -1,10 +1,13 @@
 package collectionFrameWork;
 
 import java.util.ArrayList;
-import java.util.Iterator;
+//import java.util.Iterator;
 
 public class ArrayListClass {
 	public static void main(String args[]) {
+		Student s1 = new Student(101,"Aparna",25);
+		Student s2 = new Student(102,"Bijay",29);
+		
 		ArrayList<String> al = new ArrayList<String>();
 		al.add("Aparna");
 		al.add("Bijay");
@@ -19,6 +22,16 @@ public class ArrayListClass {
 		for(String a1 : al) {
 		System.out.println(a1);
 		}
+//============Adding student type argument==========================
+		ArrayList<Student> st = new ArrayList<Student>();
+		st.add(s1);
+		st.add(s2);
+		
+		for(Student st1 : st) {
+			System.out.println(st1.age+" " + st1.name+" " +  st1.rollno);
+		}
+		
 	}
+			
 
 }
---
+++ com.myjavapractice.19.05.16/src/collectionFrameWork/Student.java
@@ -0,0 +1,12 @@
+package collectionFrameWork;
+
+public class Student {
+	int rollno;
+	String name;
+	int age;
+	public Student(int rollno, String name, int age) {
+		this.age = age;
+		this.rollno = rollno;
+		this.name = name;
+	}
+}
